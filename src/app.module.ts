3841b3d8d950bb64afd49b32f516a09951a2935e
Joan <70104624+itsjovdev@users.noreply.github.com>
2025-09-25 11:57:31 +0200

    quitando typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,24 +7,9 @@ import { ConvertModule } from './convert/convert.module';
 import { InvoiceModule } from './invoice/invoice.module';
 import { PdfModule } from './pdf/pdf.module';
 
-const USE_DB = process.env.USE_DB !== '0'; // 👈 si pones USE_DB=0, no carga TypeORM
-
 @Module({
   imports: [
-    ...(USE_DB
-      ? [
-          TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: process.env.DB_HOST!,
-            port: +(process.env.DB_PORT!),
-            username: process.env.DB_USER!,
-            password: process.env.DB_PASS!,
-            database: process.env.DB_NAME!,
-            autoLoadEntities: true,
-            synchronize: process.env.TYPEORM_SYNC === 'true',
-          }),
-        ]
-      : []),
+    // 👇 SOLO tus módulos reales, sin TypeORM
     ConvertModule,
     InvoiceModule,
     PdfModule,
